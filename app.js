83703fccda1fa2cc5e887c4e738227a145dd8c43
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-08-24 21:52:09 -0400

    heroku_deploy

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 // variable to access transactions data 
-const transactionsController = require("./controllers/transactionsController")
+const transactionsController = require("./controllers/transactionsController.js")
 
 // variable to access transactions data from models in transactionsController
 app.use("/transactions", transactionsController);
